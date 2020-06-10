fd6138e59ff95b902731bbb28cc7ae5e57cc8a82
Diana Tordoff <40304362+dianatordoff@users.noreply.github.com>
2020-06-09 17:53:25 -0700

    added files

---
+++ clusters.R
@@ -0,0 +1,71 @@
+#load packages
+library(seqinr)
+library(ape)
+library(igraph)
+library(purrr)
+library(tibble)
+library(Hmisc)
+library(memisc)
+library(stringr)
+
+#alignment <- read.fasta("PRRT.trimmed.KC.aligned.fasta")
+dna <- read.dna("PRRT.trimmed.KC.aligned.fasta", format="fasta")
+metadata <- readRDS(file = "metadata.rds")
+
+
+#Use Tamura-Nei (TN93) pairwise distance, R function from "ape" package
+class(dna) #must be a DNAbin object
+
+cluster_by_TN93 <- function(dna, min_size = 2, threshold = 0.02) {
+  TN93.dist     <- ape::dist.dna(dna, model="TN93", pairwise.deletion = TRUE, as.matrix=TRUE)
+  adj_mat       <- TN93.dist <= threshold
+  cluster_graph <- igraph::graph_from_adjacency_matrix(adj_mat, mode="undirected", diag=FALSE)
+  components    <- igraph::components(cluster_graph)
+  retain_assignments <- purrr::keep(components$membership, ~ components$csize[.x] >= min_size)
+  clusters           <- tibble::tibble(sequence_name = names(retain_assignments), cluster = retain_assignments)
+  return(clusters)
+}
+
+TN93.02 <- cluster_by_TN93(dna, min_size = 2, threshold = 0.02) 
+names(TN93.02) <- c("seqID", "clusterID")
+describe(TN93.02$clusterID)
+
+#Note: Clusters were identified based on individual sequences. 
+#      Therefore, we must account for clusters composed soley of the same individual, 
+#      and define clusters based on 2 or more unique individuals. 
+
+#deduplicate
+seqID_to_newnum <- metadata[metadata$seqID %in% names(alignment), c("seqID", "newnum")]
+TN93.02$seqID <- with(TN93.02, str_remove_all(seqID , "[']"))
+TN93.02 <- merge(TN93.02, seqID_to_newnum, by="seqID", all.x=TRUE)
+
+#create list of newnums with >1 clusterID
+TN93.02$count <-1
+unique <- with(TN93.02, aggregate(count, by=list(newnum, clusterID), FUN=sum))
+names(unique) <- c("newnum", "cluster", "N")
+unique$dup <- duplicated(unique$newnum)
+unique$dup <- (unique$newnum %in% unique[unique$dup==TRUE, "newnum"])
+unique <- subset(unique, dup==TRUE, select=c("newnum", "cluster"))
+
+#create cross walk for combing clusters
+unique$n <- ave(unique$cluster, unique$newnum, FUN = seq_along) # create index!
+unique <- reshape(unique, direction="wide", timevar = "n", idvar = "newnum") # reshape wide!
+l <- list(unique[!is.na(unique$cluster.2),c("cluster.1", "cluster.2")], 
+          unique[!is.na(unique$cluster.3),c("cluster.1", "cluster.3")])
+xwalk <- rbindlist(l, use.names=FALSE)
+names(xwalk) <- c("updated", "clusterID")
+
+#update clusters
+TN93.02 <- merge(TN93.02, xwalk, by="clusterID", all.x=TRUE )
+TN93.02$final <- with(TN93.02, ifelse(is.na(updated), clusterID, updated))
+TN93.02 <- TN93.02[,c("newnum", "final")]
+names(TN93.02) <- c("newnum", "clusterID")
+
+#check
+describe(TN93.02$newnum) 
+TN93.02 <- TN93.02[,1:2]
+rm(unique, l, xwalk)
+
+#create variable is.clustered and save
+TN93.02$is.clustered.tn02 <- 1
+write.csv(TN93.02, "TN93.02.csv")
---
+++ sequenceprocessing.R
@@ -0,0 +1,30 @@
+#load packages
+library(seqinr)
+
+#Load data
+fastafile <- read.fasta(file = "sequences.fasta")
+metadata <- readRDS(file = "metadata.rds")
+
+#Keep PR/RT (protease/reverse transcriptase)
+seqPRRT  <- fastafile[c(which(names(fastafile) %in% metadata[metadata$type=="PR/RT",]$seqID))]
+
+#Trim Sequences Lengths
+#Drop sequences <917 bp:
+lengthPRRT <- as.data.frame(cbind(getLength(seqPRRT), names(seqPRRT)))
+names(lengthPRRT) <- c("l", "seqID")
+lengthPRRT$l <- as.numeric(lengthPRRT$l)
+summary(lengthPRRT$l)
+seqPRRT  <- seqPRRT[c(which(names(seqPRRT) %in% lengthPRRT[lengthPRRT$l>=917,]$seqID))] 
+
+#and trim remaining sequences to 917 bp:
+trimmedPRRT <- seqPRRT 
+for (i in 1:length(seqPRRT)) {
+  trimmedPRRT[[i]] <- seqPRRT[[i]][0:917]
+}
+
+#Limit to King County Only 
+KCtrimmedPRRT  <- trimmedPRRT[c(which(names(trimmedPRRT) %in% metadata[metadata$KC==1,]$seqID))]
+write.fasta(KCtrimmedPRRT, names=names(KCtrimmedPRRT), file.out="PRRT.trimmed.KC.fasta")
+
+#create alignment using MAFFT, see https://mafft.cbrc.jp/alignment/software/
+#note: alignment subsequently visually inspected for accuracy in Geneious, see https://www.geneious.com/ 
